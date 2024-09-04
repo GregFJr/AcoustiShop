d4b99a26f00dd8b3a71a58b5ab9c823c349f1477
GregFJr <finney101690@gmail.com>
2024-09-04 09:51:07 -0700

    updated headphone page structure, added product gallery

--- src/app/headphones/page.tsx
+++ src/app/headphones/page.tsx
@@ -107,6 +107,26 @@ export default function Headphones() {
           <img src={product2.image.desktop} alt={product2.name} />
           </div>
         )}
+
+        <div className='audio-categories'>
+          <div>
+          <img src="../../assets/shared/desktop/image-category-thumbnail-headphones.png" alt="thumdnail headphones" />
+            <h5>Headphones</h5>
+            <a href="">Shop</a>
+          </div>
+          <div>
+          <img src="../../assets/shared/desktop/image-category-thumbnail-speakers.png" alt="thumdnail speakers" />
+          <h5>Speakers</h5>
+          <a href="">Shop</a>
+          </div>
+          <div>
+          <img src="../../assets/shared/desktop/image-category-thumbnail-earphones.png" alt="thumdnail earphones" />
+          <h5>Earphones</h5>
+          <a href="">Shop</a>
+          </div>
+        </div>
+
+        <div>lanloanlsnolknas</div>
       </section>
     </div>
   );
